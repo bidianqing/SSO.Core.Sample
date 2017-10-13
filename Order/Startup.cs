bc0cf7678bf4f5278825bdbef1528578d753d333
毕殿卿 <bdq123@outlook.com>
2017-10-13 22:22:47 +0800

    *

--- Order/Startup.cs
+++ Order/Startup.cs
@@ -26,6 +26,7 @@ namespace Order
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDataProtection(options => options.ApplicationDiscriminator = "oneaspnet");
             services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
